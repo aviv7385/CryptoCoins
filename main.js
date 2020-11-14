ed1f63f9511d2da27f58f3f49da534f06e6697fb
AvivE <aviv7385@gmail.com>
2020-11-14 19:51:25 +0200

    display the ABOUT-ME section and added nav bar

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /// <reference path="jquery-3.5.1.js" />
 
 function getCoins() {
+    //clear previous content
+    $(".cards-container").empty()
     //get the Promise object by calling the function where the Promise object was created
     //and give the desired url as an object
     getJsonFromServer("https://api.coingecko.com/api/v3/coins/list")
@@ -31,7 +33,7 @@ function displayCoins(cryptRequest) {
                 </div>
                 <div class="col">
                     <div class="custom-control custom-switch mySwitch">
-                        <input type="checkbox" class="custom-control-input" id="customSwitch${i}">
+                        <input type="checkbox" class="custom-control-input toggleCheckbox" id="customSwitch${i}">
                         <label class="custom-control-label" for="customSwitch${i}"></label>
                     </div>
                 </div>
@@ -44,13 +46,13 @@ function displayCoins(cryptRequest) {
             </div>
             
             <div class="row">
-                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample${i}" aria-expanded="false" aria-controls="collapseExample${i}">
+                <button onclick="getMoreInfo(id)" id="${cryptRequest[i].id}" class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample${i}" aria-expanded="false" aria-controls="collapseExample${i}">
                     More Info
                 </button>
         
                 <div class="collapse" id="collapseExample${i}">
-                    <div class="card card-body">
-                        
+                    <div class="card card-body moreInfo">
+                    
                     </div>
                 </div>
             </div>
@@ -65,15 +67,27 @@ function getMoreInfo(id) {
         url: `https://api.coingecko.com/api/v3/coins/${id}`,
         error: err => alert(err),
         success: infoRequest => {
-            $(".collapse > .card-body").html(
-                `USD: $${infoRequest.market_data.current_price.usd}<br>
-                 EUR: &#8352;${infoRequest.market_data.current_price.eur}<br>
-                 ILS: ${infoRequest.market_data.current_price.ils}&#8362;<br>`
+            $(".moreInfo").html(
+                `<img class="coinPic" src="${infoRequest.image.small}"><br>
+                USD: $${infoRequest.market_data.current_price.usd}<br>
+                EUR: &euro;${infoRequest.market_data.current_price.eur}<br>
+                ILS: ${infoRequest.market_data.current_price.ils}&#8362;<br>`
             )
         }
     });
 }
 
-getCoins()
+//on page load, call this function
+getCoins();
 
-//$(".btn").on("click", getMoreInfo(cryptRequest[i].name));
+// a function to display the "about" page
+function displayAboutMe() {
+    //remove previous content and insert new content
+    $(".cards-container").empty().html(`
+    <h3 class="aboutHead">ABOUT ME - AVIV ELAD</h3>
+    <p class="aboutPar">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis. Velit laoreet id donec ultrices tincidunt arcu non sodales neque. Ornare arcu odio ut sem. Lorem donec massa sapien faucibus et molestie ac. Eu volutpat odio facilisis mauris sit. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Laoreet id donec ultrices tincidunt arcu non sodales neque. Duis tristique sollicitudin nibh sit amet commodo nulla. Risus nec feugiat in fermentum posuere urna. Vulputate enim nulla aliquet porttitor lacus luctus accumsan. Felis imperdiet proin fermentum leo vel orci porta. Non arcu risus quis varius. Imperdiet proin fermentum leo vel orci porta non. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Massa id neque aliquam vestibulum morbi. Elit duis tristique sollicitudin nibh sit. Sed elementum tempus egestas sed sed risus.</p>
+    <p class="aboutPar">Feugiat scelerisque varius morbi enim. Id nibh tortor id aliquet lectus. Vitae semper quis lectus nulla at. In massa tempor nec feugiat nisl pretium fusce. Dignissim suspendisse in est ante in nibh. Integer quis auctor elit sed vulputate mi sit amet. Quisque non tellus orci ac auctor augue mauris. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Nisi lacus sed viverra tellus in. Non enim praesent elementum facilisis leo vel. Morbi leo urna molestie at elementum eu facilisis.</p>
+    `)
+}
+
+
