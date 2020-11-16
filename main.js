12236b68b24ae87b552e0b79dfa44edd14928380
AvivE <aviv7385@gmail.com>
2020-11-16 19:50:12 +0200

    added bootstrap responsive layout and my picture

--- main.js
+++ main.js
@@ -25,13 +25,14 @@ function getJsonFromServer(jsonUrl) {
 //this function gets an object as an argument and will display the wanted information from that object 
 function displayCoins(cryptRequest) {
     for (let i = 0; i < 300; i++) {
-        $(".cards-container").append(`<div class="card coinCard">
+        $(".cards-container").append(`
+        <div class="card coinCard col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
         <div class="card-body">   
             <div class="row">
-                <div class="col">
+                <div class="col" id="coinSymbol">
                     <h5 class="card-title">${cryptRequest[i].symbol}</h5>
                 </div>
-                <div class="col">
+                <div class="col" id="coinToggle">
                     <div class="custom-control custom-switch mySwitch">
                         <input type="checkbox" class="custom-control-input toggleCheckbox" id="customSwitch${i}">
                         <label class="custom-control-label" for="customSwitch${i}"></label>
@@ -40,13 +41,13 @@ function displayCoins(cryptRequest) {
             </div>
             
             <div class="row">
-                <div class="col">
+                <div class="col" id="coinName">
                     <p class="card-text">${cryptRequest[i].name}</p><br>
                 </div>
             </div>
             
             <div class="row">
-                <button onclick="getMoreInfo(id)" id="${cryptRequest[i].id}" class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample${i}" aria-expanded="false" aria-controls="collapseExample${i}">
+                <button onclick="getMoreInfo(id)" id="${cryptRequest[i].id}" class="btn btn-primary moreInfoBtn" type="button" data-toggle="collapse" data-target="#collapseExample${i}" aria-expanded="false" aria-controls="collapseExample${i}">
                     More Info
                 </button>
         
@@ -81,13 +82,22 @@ function getMoreInfo(id) {
 getCoins();
 
 // a function to display the "about" page
+
 function displayAboutMe() {
     //remove previous content and insert new content
-    $(".cards-container").empty().html(`
-    <h3 class="aboutHead">ABOUT ME - AVIV ELAD</h3>
-    <p class="aboutPar">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis. Velit laoreet id donec ultrices tincidunt arcu non sodales neque. Ornare arcu odio ut sem. Lorem donec massa sapien faucibus et molestie ac. Eu volutpat odio facilisis mauris sit. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Laoreet id donec ultrices tincidunt arcu non sodales neque. Duis tristique sollicitudin nibh sit amet commodo nulla. Risus nec feugiat in fermentum posuere urna. Vulputate enim nulla aliquet porttitor lacus luctus accumsan. Felis imperdiet proin fermentum leo vel orci porta. Non arcu risus quis varius. Imperdiet proin fermentum leo vel orci porta non. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Massa id neque aliquam vestibulum morbi. Elit duis tristique sollicitudin nibh sit. Sed elementum tempus egestas sed sed risus.</p>
-    <p class="aboutPar">Feugiat scelerisque varius morbi enim. Id nibh tortor id aliquet lectus. Vitae semper quis lectus nulla at. In massa tempor nec feugiat nisl pretium fusce. Dignissim suspendisse in est ante in nibh. Integer quis auctor elit sed vulputate mi sit amet. Quisque non tellus orci ac auctor augue mauris. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Nisi lacus sed viverra tellus in. Non enim praesent elementum facilisis leo vel. Morbi leo urna molestie at elementum eu facilisis.</p>
+    $(".cards-container").empty()
+    $(".cards-container").append(`
+    <div class="aboutMe col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <h3 class="aboutMeHead">ABOUT ME: AVIV ELAD</h3>
+        <img src="Assets/images/my pic.jpg" id="myPic">
+        <p class="aboutMePar">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis. Velit laoreet id donec ultrices tincidunt arcu non sodales neque. Ornare arcu odio ut sem. Lorem donec massa sapien faucibus et molestie ac. Eu volutpat odio facilisis mauris sit. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Laoreet id donec ultrices tincidunt arcu non sodales neque. Duis tristique sollicitudin nibh sit amet commodo nulla. Risus nec feugiat in fermentum posuere urna. Vulputate enim nulla aliquet porttitor lacus luctus accumsan. Felis imperdiet proin fermentum leo vel orci porta. Non arcu risus quis varius. Imperdiet proin fermentum leo vel orci porta non. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Massa id neque aliquam vestibulum morbi. Elit duis tristique sollicitudin nibh sit. Sed elementum tempus egestas sed sed risus.</p>
+    </div>
+    <div class="aboutProject col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <h3 class="aboutProHead">ABOUT THE PROJECT</h3>
+        <p class="aboutProPar">Feugiat scelerisque varius morbi enim. Id nibh tortor id aliquet lectus. Vitae semper quis lectus nulla at. In massa tempor nec feugiat nisl pretium fusce. Dignissim suspendisse in est ante in nibh. Integer quis auctor elit sed vulputate mi sit amet. Quisque non tellus orci ac auctor augue mauris. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Nisi lacus sed viverra tellus in. Non enim praesent elementum facilisis leo vel. Morbi leo urna molestie at elementum eu facilisis.</p>
+    </div>
     `)
+
 }
 
 
